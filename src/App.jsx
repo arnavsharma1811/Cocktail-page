ab77de4b6725b95df6b6f227bf5541b209d32e8c
arnavsharma1811 <arnavsharmaone8@gmail.com>
2025-11-26 16:08:22 +0530

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Hero from './components/Hero.jsx'
+import Navbar from './components/navbar.jsx'
+const App = () => {
+    return(
+        <>
+        <Navbar/>
+        <Hero/>
+        </>
+    )
+}
+
+export default App
