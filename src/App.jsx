9a9bdd1b97e64565560ca38da0efe258e351aa97
arnavsharma1811 <arnavsharmaone8@gmail.com>
2025-11-30 03:07:04 +0530

    fixed a typo bug

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import About from './components/About.jsx'
 import Art from './components/Art.jsx'
 import Cocktails from './components/Cocktails.jsx'
 import Hero from './components/Hero.jsx'
-import Navbar from './components/navbar.jsx'
+import Navbar from './components/Navbar.jsx'
 import Menu from './components/Menu.jsx'
 import Contact from './components/Contact.jsx'
 const App = () => {
