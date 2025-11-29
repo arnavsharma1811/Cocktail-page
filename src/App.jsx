e86fbe0c8914eac249681f0f6f28f5753b1b0753
arnavsharma1811 <arnavsharmaone8@gmail.com>
2025-11-30 02:33:59 +0530

    implemented Menu section

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Art from './components/Art.jsx'
 import Cocktails from './components/Cocktails.jsx'
 import Hero from './components/Hero.jsx'
 import Navbar from './components/navbar.jsx'
+import Menu from './components/Menu.jsx'
 const App = () => {
     return(
         <>
@@ -11,6 +12,7 @@ const App = () => {
         <Cocktails/>
         <About/>
         <Art/>
+        <Menu/>
         </>
     )
 }
